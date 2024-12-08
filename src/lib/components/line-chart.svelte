0d64dc0b3ee8adb3167228a447fa0f4cf00923e6
Juri Berlanda <juriberlanda@hotmail.com>
2024-12-08 13:56:45 +0100

    [UI] Make line chart responsive.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- src/lib/components/line-chart.svelte
+++ src/lib/components/line-chart.svelte
@@ -2,6 +2,7 @@
 	import type { LoanParameters, RedemptionPlanItem } from '$lib/domain';
 
 	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
 
 	import { calculatePlan } from '$lib/logic/redemption_calculator';
 	import { formatNumber } from '$lib/utils';
@@ -17,19 +18,29 @@
 	let base = $state(calculatePlan(loanData, {}));
 	let current = $state(calculatePlan(loanData, unscheduledRepayments));
 	let projected = $state(calculatePlan(loanData, { ...unscheduledRepayments, ...plannedRepayments }));
+	let width = $state(1280);
 
-	const width = 1280;
-	const height = 800;
-	const marginTop = 20;
-	const marginRight = 20;
 	const marginBottom = 30;
 	const marginLeft = 60;
 
 	let gx: SVGGElement;
 	let gy: SVGGElement;
+	let container: HTMLElement;
 
-	let x = $derived(d3.scaleUtc([base.plan[0].date, base.last.date], [marginLeft, width - marginRight]));
-	let y = $derived(d3.scaleLinear([0, loanData.amount], [height - marginBottom, marginTop]));
+	onMount(() => {
+		const resizeObserver = new ResizeObserver(entries => {
+			const entry = entries.at(0);
+			if (entry) {
+				width = entry.contentRect.width;
+			}
+		});
+		resizeObserver.observe(container);
+		return () => resizeObserver.unobserve(container);
+	});
+
+	let height = $derived(width * 10 / 16);
+	let x = $derived(d3.scaleUtc([base.plan[0].date, base.last.date], [marginLeft, width - 20]));
+	let y = $derived(d3.scaleLinear([0, loanData.amount], [height - 30, 20]));
 
 	let line = $derived(
 		d3.line<RedemptionPlanItem>(
@@ -47,32 +58,34 @@
 	});
 </script>
 
-<svg {width} {height}>
-	<g bind:this={gx} transform="translate(0,{height - marginBottom})" />
-	<g bind:this={gy} transform="translate({marginLeft},0)" />
+<div bind:this={container} class="container">
+	<svg {width} {height}>
+		<g bind:this={gx} transform="translate(0,{height - marginBottom})" />
+		<g bind:this={gy} transform="translate({marginLeft},0)" />
 
-	<path fill="none" stroke="#017FC0" stroke-width="1.5" d={today} />
+		<path fill="none" stroke="#017FC0" stroke-width="1.5" d={today} />
 
-	<path fill="none" stroke="#D93526" stroke-width="1.5" d={line(base.plan)} />
-	<path fill="none" stroke="#398712" stroke-width="1.5" d={line(current.plan)} />
-	<path fill="none" stroke="#977000" stroke-width="1.5" d={line(projected.plan)} />
-</svg>
+		<path fill="none" stroke="#D93526" stroke-width="1.5" d={line(base.plan)} />
+		<path fill="none" stroke="#398712" stroke-width="1.5" d={line(current.plan)} />
+		<path fill="none" stroke="#977000" stroke-width="1.5" d={line(projected.plan)} />
+	</svg>
 
-<table style="max-width: {width}px">
-	<thead>
-		<tr>
-			<th></th>
-			<th scope="col">End</th>
-			<th scope="col">Sum paid</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#each [{ label: 'Base', plan: base }, { label: 'Current', plan: current }, { label: 'Projected', plan: projected }] as item}
+	<table style="max-width: {width}px">
+		<thead>
+			<tr>
+				<th></th>
+				<th scope="col">End</th>
+				<th scope="col">Sum paid</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each [{ label: 'Base', plan: base }, { label: 'Current', plan: current }, { label: 'Projected', plan: projected }] as item}
 			<tr>
 				<th scope="row">{item.label}</th>
 				<td>{item.plan.last.date.toLocaleDateString()}</td>
 				<td>{formatNumber(item.plan.sumInstalments)} €</td>
 			</tr>
-		{/each}
-	</tbody>
-</table>
+			{/each}
+		</tbody>
+	</table>
+</div>
