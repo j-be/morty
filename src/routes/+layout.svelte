f9df560512ef1356aad66649626b27d32f919525
Juri Berlanda <juriberlanda@hotmail.com>
2024-12-08 13:08:35 +0100

    [Style] Add PicoCSS.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.sass';
+</script>
+
+<nav>
+	<div class="container">
+		<h1>Morty</h1>
+	</div>
+</nav>
+<main class="container">
+	<slot />
+</main>
+<footer>
+	<div class="container">
+		Juri Berlanda<br />
+		Bahnstra&szlig;e 52 / Haus 13, 2345 Brunn am Gebirge
+	</div>
+</footer>
